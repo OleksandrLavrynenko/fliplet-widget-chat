8e7e852b39218cf48016b322ddbd52d99fcb581c
OleksandrLavrynenko <86256215+OleksandrLavrynenko@users.noreply.github.com>
2021-09-22 11:32:28 +0300

    OD-531 [FIX] Fixed error of displaying the datasource provider  and the link provider  (#114)
    * [FIX] add event  'show-widget'
    
    * [FIX] fixed ivent name
    
    * [FIX] fixed event name
    
    * [FIX] small fix
    
    * [FIX] fix lint

--- js/interface.js
+++ js/interface.js
@@ -75,6 +75,14 @@ $('#show-full-name-field').on('click', function() {
   $('.first-last-names-holder').addClass('hidden');
 });
 
+$('#chat-contacts-settings').on('shown.bs.tab', function() {
+  dataSourceProvider.emit('widget-autosize');
+});
+
+$('#chat-security-settings').on('shown.bs.tab', function() {
+  linkSecurityProvider.emit('widget-autosize');
+});
+
 // Fired from Fliplet Studio when the external save button is clicked
 Fliplet.Widget.onSaveRequest(function() {
   dataSourceProvider.forwardSaveRequest();
